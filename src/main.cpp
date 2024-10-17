757af152cdbf47831d927aa71e242730602e299c
joshika.namani <joshika.namani@braneenterprises.com>
2024-10-17 17:08:45 +0530

    integrated Atomic Example

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,53 @@
 using namespace std;
 // Creation of Threads
 
+int num = 20,x=5, y =30;
+
 // Funtion thread
 void funcThread(){
-  cout<<"[Creating threading using function] ID "<<this_thread::get_id()<<endl;
-}
+    static int var = 0;
+    var++;
+    thread_local int t_local = 0;
+    t_local++;
 
-// Lambda thread
-auto lambdaThread = []{ cout<<"[Creating threading using Lambda] "<<this_thread::get_id()<<endl; };
+    num = t_local + var;
+
+    if (x == 5) // The "Check"
+    {
+    y = x * 2; // The "Act"
+    cout<<"Y "<<y<<endl;
+    x = y;
+
+   // If another thread changed x in between "if (x == 5)" and "y = x * 2" above,
+   // y will not be equal to 10.
+    }
+    /*If you want to have 'static' variables that are static within the scope of each particular thread, 
+    use thread_local variables instead. Then each thread will have its own version of the static variable, and the 
+    static variable will only be destroyed on thread exit. */
+
+   cout<<"[Creating threading using function] ID "<<this_thread::get_id()<<" t_local: "<<t_local<<" num: "<<num<<endl;}
+
+    // Lambda thread
+    auto lambdaThread = []{ 
+    num = 300;
+    cout<<"X "<<x<<endl;
+    if (x == 5) // The "Check"
+    {
+    y = x * 2; // The "Act"
+    cout<<"Y "<<y<<endl;
+
+   // If another thread changed x in between "if (x == 5)" and "y = x * 2" above,
+   // y will not be equal to 10.
+    }
+    cout<<"[Creating threading using Lambda] "<<this_thread::get_id()<<" : "<<num<<endl; };
 
 //Functor
 class thread_Class{
-    public:
-    void operator()() {
-        cout << "[Creating threading using Functor] "<<this_thread::get_id()<< endl;
-    }
 
+    public:
+      void operator()() {
+      cout << "[Creating threading using Functor] "<<this_thread::get_id()<< endl;
+      }
 };
 
 int main(){
