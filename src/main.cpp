f7fb3bcef5b9fce5e1ff7c992ab900fdb28f8c15
joshika.namani <joshika.namani@braneenterprises.com>
2024-10-17 14:02:18 +0530

    Thread Creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,40 @@ using namespace std;
 
 // Funtion thread
 void funcThread(){
-  cout<<"[Creating threading using function]"<<endl;
+  cout<<"[Creating threading using function] ID "<<this_thread::get_id()<<endl;
 }
 
 // Lambda thread
-auto lambdaThread = []{ cout<<"[Creating threading using Lambda]"<<endl; };
+auto lambdaThread = []{ cout<<"[Creating threading using Lambda] "<<this_thread::get_id()<<endl; };
 
 //Functor
 class thread_Class{
-      void functors() {
-        cout<<"[Creating threading using Lambda]"<<endl;
-      }
+    public:
+    void operator()() {
+        cout << "[Creating threading using Functor] "<<this_thread::get_id()<< endl;
+    }
 
 };
 
 int main(){
 
-    thread thread_function(funcThread);
-    thread_function.join();
+    cout<<"WELCOME TO Multi-Threading "<<endl;
+
+    unsigned int c = std::thread::hardware_concurrency();
+
+    cout<<"Number of threads we can create "<<c<<endl;
 
     thread thread_function(funcThread);
     thread_function.join();
 
-    thread thread_function(funcThread);
-    thread_function.join();
+    thread thread_lambda(lambdaThread);
+    thread_lambda.join();
+  
+    // Thread using a functor (ensure to create a proper object)
+    thread_Class functor;
+    thread thread_functors(functor);  // Pass the functor object to the thread
+    thread_functors.join();  // Join the thread
     
-    cout<<"WELCOME TO MULTI-THREADING TUTORIAL "<<endl;
     return 0;
 
 }
